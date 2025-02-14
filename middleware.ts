906b12670f8ddb030189191ca643c1a985165f83
PHUWADON SUWANNARAT <107117090+PHUWADON-PHUDON@users.noreply.github.com>
2025-02-14 22:18:59 +0700

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,3 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function middleware(request: NextRequest) {
-    const token = request.cookies.get("next-auth.session-token")?.value;
+export { default } from "next-auth/middleware"
 
-    if (token) {
-        return NextResponse.next();
-    }
-
-    return NextResponse.redirect(new URL('/', request.url))
-}
- 
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: ['/favorits/:path*','/camp/:path*'],
-}
+export const config = { matcher: ['/favorits/:path*','/camp/:path*'] }
